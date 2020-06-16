7d9065fdbb9dba277f72b8d706aaf3f920a451a6
meego <meego@diprime.com>
2020-06-16 14:11:45 +0300

    Add bind to Telegram::class. Telegram from container will be always authenticated.

--- src/TelegramServiceProvider.php
+++ src/TelegramServiceProvider.php
@@ -15,14 +15,11 @@ class TelegramServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->app->when(TelegramChannel::class)
-            ->needs(Telegram::class)
-            ->give(static function () {
-                return new Telegram(
-                    config('services.telegram-bot-api.token'),
-                    app(HttpClient::class),
-                    config('services.telegram-bot-api.base_uri')
-                );
-            });
+        $this->app->bind(Telegram::class, static function () {
+            return new Telegram(
+                config('services.telegram-bot-api.token'),
+                new HttpClient()
+            );
+        });
     }
 }
